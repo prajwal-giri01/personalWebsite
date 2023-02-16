4e40c18657354448b93aeced049622179b3b65e1
prajwal-giri01 <117528397+prajwal-giri01@users.noreply.github.com>
2023-02-16 21:13:22 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function openmenu(){
+    document.getElementById("sidemenu").style.display = "flex";
+}
+
+function closemenu(){
+    document.getElementById("sidemenu").style.display="none"
+}
+function showAlert() {
+    alert("The form was submitted");
+  }
